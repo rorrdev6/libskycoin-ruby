5fd8e446b8c1bdcd7ee4a04dabae5b078d13acc4
rorrdev6 <rorrdevpro@proton.me>
2019-03-21 15:14:23 -0400

    [skyapi] refs #5 Install bundler for travis

--- travis/skyapi.sh
+++ travis/skyapi.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+gem install bundler:1.16.1
+bundle update --bundler
 cd $TRAVIS_BUILD_DIR/lib/skyapi
 (cd $TRAVIS_BUILD_DIR/lib/skyapi && bundle install)
 (cd $TRAVIS_BUILD_DIR/lib/skyapi && gem build sky_api.gemspec)
