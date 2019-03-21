e17dab9a6a1efa6259d428d92df18bdd5078c4ba
rorrdev6 <rorrdevpro@proton.me>
2019-03-21 14:58:38 -0400

    [skyapi] refs #5 Update skyapi.sh travis

--- travis/skyapi.sh
+++ travis/skyapi.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd $TRAVIS_BUILD_DIR/lib/skyapi
-bundle install
-gem build sky_api.gemspec
-rake
+(cd $TRAVIS_BUILD_DIR/lib/skyapi && bundle install)
+(cd $TRAVIS_BUILD_DIR/lib/skyapi && gem build sky_api.gemspec)
+(cd $TRAVIS_BUILD_DIR/lib/skyapi && rake)
