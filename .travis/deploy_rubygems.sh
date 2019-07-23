994c124edebf780666100c97134fa199a874a7c8
rorrdev6 <rorrdevpro@proton.me>
2019-07-22 21:04:54 -0400

    [ci] refs #17 Update package location in deploy

--- .travis/deploy_rubygems.sh
+++ .travis/deploy_rubygems.sh
@@ -10,5 +10,5 @@ echo ":rubygems_api_key: " + "$RUBYGEMS_API_KEY" >> ~/.gem/credentials
 chmod 0600 ~/.gem/credentials
 
 # Deploy to rubygems
-gem push lib/skyapi/pkg/sky_api-${TRAVIS_TAG}.gem
-gem push lib/skycoin/pkg/skycoin-${TRAVIS_TAG}.gem
+gem push lib/skyapi/sky_api-${TRAVIS_TAG}.gem
+gem push lib/skycoin/skycoin-${TRAVIS_TAG}.gem
